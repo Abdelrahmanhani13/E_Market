f5b8f394dd26c3f01f58ba3b1f57bf96ff4bead9
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2025-11-22 16:50:36 +0200

    create custom text field

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -7,13 +7,46 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           children: [
             Gap(100),
-            Text('Welcome to E Market'),
-            Gap(20),
-            Text('Login'),
+            Text(
+              'Welcome to E Market',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+            Gap(50),
+            Card(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Gap(30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      // controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: Colors.grey[300]!),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
