6680a77a0b02421f4f7e4221c8ffc83ee769f48d
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2025-11-23 23:19:54 +0200

    finish bottom nav bar UI

--- lib/features/nav_bar/presentation/views/bottom_nav_bar.dart
+++ lib/features/nav_bar/presentation/views/bottom_nav_bar.dart
@@ -1,3 +1,4 @@
+import 'package:e_market/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
@@ -8,43 +9,35 @@ class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: GNav(
-        rippleColor: Colors.grey[800]!, // tab button ripple color when pressed
-        hoverColor: Colors.grey[700]!, // tab button hover color
-        haptic: true, // haptic feedback
-        tabBorderRadius: 15,
-        tabActiveBorder: Border.all(
-          color: Colors.black,
-          width: 1,
-        ), // tab button border
-        tabBorder: Border.all(
-          color: Colors.grey,
-          width: 1,
-        ), // tab button border
-        tabShadow: [
-          // ignore: deprecated_member_use
-          BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8),
-        ], // tab button shadow
-        curve: Curves.easeOutExpo, // tab animation curves
-        duration: Duration(milliseconds: 900), // tab animation duration
-        gap: 8, // the tab button gap between icon and text
-        color: Colors.grey[800], // unselected icon color
-        activeColor: Colors.purple, // selected icon and text color
-        iconSize: 24, // tab button icon size
-        // ignore: deprecated_member_use
-        tabBackgroundColor: Colors.purple.withOpacity(
-          0.1,
-        ), // selected tab background color
-        padding: EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 5,
-        ), // navigation bar padding
-        tabs: [
-          GButton(icon: LineIcons.home, text: 'Home'),
-          GButton(icon: LineIcons.store, text: 'Likes'),
-          GButton(icon: LineIcons.heart, text: 'Search'),
-          GButton(icon: LineIcons.user, text: 'Profile'),
-        ],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: Colors.white),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: GNav(
+            rippleColor:
+                AppColors.primaryColor, // tab button ripple color when pressed
+            hoverColor: AppColors.primaryColor, // tab button hover color
+            tabBorderRadius: 15, // tab button border // tab button shadow
+            duration: Duration(milliseconds: 400), // tab animation duration
+            gap: 8, // the tab button gap between icon and text
+            color: Colors.grey, // unselected icon color
+            activeColor: Colors.white, // selected icon and text color
+            iconSize: 24, // tab button icon size
+            // ignore: deprecated_member_use
+            tabBackgroundColor:
+                AppColors.primaryColor, // selected tab background color
+            padding: EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 8,
+            ), // navigation bar padding
+            tabs: [
+              GButton(icon: LineIcons.home, text: 'Home'),
+              GButton(icon: LineIcons.store, text: 'Likes'),
+              GButton(icon: LineIcons.heart, text: 'Search'),
+              GButton(icon: LineIcons.user, text: 'Profile'),
+            ],
+          ),
+        ),
       ),
     );
   }
