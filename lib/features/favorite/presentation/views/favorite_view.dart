5d633c8d60a5cd8140b5596c33bc69ecc9a8b430
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2025-11-24 02:34:37 +0200

    create favorite products

--- lib/features/favorite/presentation/views/favorite_view.dart
+++ lib/features/favorite/presentation/views/favorite_view.dart
@@ -1,4 +1,6 @@
+import 'package:e_market/features/home/presentation/widgets/product_card.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class FavoriteView extends StatelessWidget {
   const FavoriteView({super.key});
@@ -7,10 +9,26 @@ class FavoriteView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Favorite'),
+        title: Center(child: const Text('Favorite Products')),
+        surfaceTintColor: Colors.white,
+        backgroundColor: Colors.white,
       ),
-      body: const Center(
-        child: Text('Favorite View'),
+      body: ListView(
+        children: [
+          const Gap(20),
+
+          // Fixed: Using shrinkWrap and physics
+          ListView.builder(
+            shrinkWrap: true, // Important: allows ListView to size itself
+            physics:
+                const NeverScrollableScrollPhysics(), // Disables inner scroll
+            padding: const EdgeInsets.only(top: 15),
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return const ProductCard();
+            },
+          ),
+        ],
       ),
     );
   }
