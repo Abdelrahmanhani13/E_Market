89589bb2cbbdffc44ba61247060c46633a8ab8aa
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2025-11-24 14:22:02 +0200

    initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:e_market/features/nav_bar/presentation/views/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://vlsmvtmxeceraphscpma.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsc212dG14ZWNlcmFwaHNjcG1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NzY5NTAsImV4cCI6MjA3OTU1Mjk1MH0.fJPsakBH6298YVByce7VufvTata7b_CNdZvUvAzC0FQ',
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,9 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'E Market',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white
-      ),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       home: BottomNavBar(),
     );
   }
